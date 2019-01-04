e87ccb7aef6eff1011ef8e23b1c16e1560632284
Zac Robinson <zacharyleerobinson@gmail.com>
2019-01-03 22:06:11 -0500

    Added Jenkins install script.

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-
 # Must be ran as sudo
+
+# Check if Java is installed
+if ! type -p java; then
+    echo "Java is not installed."
+    exit 1
+fi
+
+# Jenkins install steps
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list
 apt-get update
